0447820198faaaa2681033896ce32daf0127a6b3
Sebmarine <cbassnotw@gmail.com>
2023-02-13 15:27:52 -0800

    Made RPS game on service page, need css for it

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "../../App.css";
+import RPS from "../RPS";
 
-export default function Services() {
-  return <h1 className="services">Services</h1>;
+function Services() {
+  return (
+    <>
+      <h1 className="services">Services</h1>
+      <RPS />
+    </>
+  );
 }
+
+export default Services;
